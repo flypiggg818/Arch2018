ef7180e535b6370cb04215cc6ef0ea903fa7d00a
flypiggg818 <qiansucheng2017@outlook.com>
2018-11-12 10:53:16 +0800

    modify adder

--- adder/src/adder.v
+++ adder/src/adder.v
@@ -19,12 +19,59 @@ module adder(
 	//
 	// Hint: 
 	//   The module needs 4 ports, 
-	//     the first 2 ports input two 16-bit unsigned numbers as the addends
-	//     the third port outputs a 16-bit unsigned number as the sum
-	//	   the forth port outputs a 1-bit carry flag as the overflow
-	// 
+
+	//     the first 2 ports are 16-bit unsigned numbers as the inputs of the adder
+	//     the third port is a 16-bit unsigned number as the output
+	//	   the forth port is a one bit port as the carry flag
+	//
+	input wire [15:0] A, B,
+	output wire [15:0] rslt,
+	output wire cout
 );
-	// TODO: Implement this module here
-	// Hint: You can use generate statement in Verilog to create multiple instantiations of modules and code.
-	
+    wire [15:0] c_rpl;
+    reg cin; 
+    
+    assign cout = c_rpl[15]; 
+    
+    initial begin 
+    	cin = 1'b0; 
+    end 
+    
+    full_adder FA0(.in1(A[0]), .in2(B[0]), .cin(cin), .rslt(rslt[0]), .cout(c_rpl[0])); 
+    full_adder FA1(.in1(A[1]), .in2(B[1]), .cin(c_rpl[0]), .rslt(rslt[1]), .cout(c_rpl[1]));
+    full_adder FA2(.in1(A[2]), .in2(B[2]), .cin(c_rpl[1]), .rslt(rslt[2]), .cout(c_rpl[2]));
+    full_adder FA3(.in1(A[3]), .in2(B[3]), .cin(c_rpl[2]), .rslt(rslt[3]), .cout(c_rpl[3]));
+    full_adder FA4(.in1(A[4]), .in2(B[4]), .cin(c_rpl[3]), .rslt(rslt[4]), .cout(c_rpl[4]));
+    full_adder FA5(.in1(A[5]), .in2(B[5]), .cin(c_rpl[4]), .rslt(rslt[5]), .cout(c_rpl[5]));
+    full_adder FA6(.in1(A[6]), .in2(B[6]), .cin(c_rpl[5]), .rslt(rslt[6]), .cout(c_rpl[6]));
+    full_adder FA7(.in1(A[7]), .in2(B[7]), .cin(c_rpl[6]), .rslt(rslt[7]), .cout(c_rpl[7]));
+    full_adder FA8(.in1(A[8]), .in2(B[8]), .cin(c_rpl[7]), .rslt(rslt[8]), .cout(c_rpl[8]));
+    full_adder FA9(.in1(A[9]), .in2(B[9]), .cin(c_rpl[8]), .rslt(rslt[9]), .cout(c_rpl[9]));
+    full_adder FA10(.in1(A[10]), .in2(B[10]), .cin(c_rpl[9]), .rslt(rslt[10]), .cout(c_rpl[10]));
+    full_adder FA11(.in1(A[11]), .in2(B[11]), .cin(c_rpl[10]), .rslt(rslt[11]), .cout(c_rpl[11]));
+    full_adder FA12(.in1(A[12]), .in2(B[12]), .cin(c_rpl[11]), .rslt(rslt[12]), .cout(c_rpl[12]));
+    full_adder FA13(.in1(A[13]), .in2(B[13]), .cin(c_rpl[12]), .rslt(rslt[13]), .cout(c_rpl[13]));
+    full_adder FA14(.in1(A[14]), .in2(B[14]), .cin(c_rpl[13]), .rslt(rslt[14]), .cout(c_rpl[14]));
+    full_adder FA15(.in1(A[15]), .in2(B[15]), .cin(c_rpl[14]), .rslt(rslt[15]), .cout(c_rpl[15])); 
+
 endmodule
+
+/**
+sub-module used by module adder, which adds two bits and a carry together and gives 
+the corresponding result and carry. 
+*/
+module full_adder(
+    input wire in1, 
+    input wire in2, 
+    input wire cin,  
+    output reg rslt, 
+    output reg cout
+);
+    // This 'always' block will be synthesized to be logic gates. 
+    always @ (in1, in2, cin)
+    begin 
+    {cout, rslt} = in1 + in2 + cin; 
+    end
+endmodule
+
+
--- adder/src/adder2.v
+++ adder/src/adder2.v
@@ -14,16 +14,85 @@
  *
  */
 
-module adder(
-	// TODO: Write the ports of this module here
-	//
-	// Hint: 
-	//   The module needs 4 ports, 
-	//     the first 2 ports input two 16-bit unsigned numbers as the addends
-	//     the third port outputs a 16-bit unsigned number as the sum
-	//	   the forth port outputs a 1-bit carry flag as the overflow
-	// 
+module Adder(
+	input wire [15:0] A, B,
+	output wire [15:0] rslt,
+	output wire cout
+); 
+	wire [3:0] P; 
+	wire [3:0] G; 
+	wire Cin; 
+	wire [3:0] C; 
+	wire [3:0] wst; 
+	CLU_16 CLU_16_0(.P(P), .G(G), .Cin(Cin), .C(C), .Cout(cout));
+	bit4_adder FBA_0(.A(A[3:0]), .B(B[3:0]), .Cin(C[0]), .rslt(rslt[3:0]), .Cout(wst[0]), .PP(P[0]), .GG(G[0])); 
+	bit4_adder FBA_1(.A(A[7:4]), .B(B[7:4]), .Cin(C[1]), .rslt(rslt[7:4]), .Cout(wst[1]), .PP(P[1]), .GG(G[1]));
+	bit4_adder FBA_2(.A(A[11:8]), .B(B[11:8]), .Cin(C[2]), .rslt(rslt[11:8]), .Cout(wst[2]), .PP(P[2]), .GG(G[2]));
+	bit4_adder FBA_3(.A(A[15:12]), .B(B[15:12]), .Cin(C[3]), .rslt(rslt[15:12]), .Cout(wst[3]), .PP(P[3]), .GG(G[3]));
+endmodule
+
+// implement each building block
+module bit_full_adder(
+	input wire A, // operands 
+	input wire B, 
+	input wire Cin, // carry 
+	output wire P, // helper
+	output wire G, 
+	output wire S // bit sum  
+); 
+	assign P = A | B;
+	assign G = A & B; 
+	assign S = A ^ B ^ Cin; 
+endmodule
+
+module CLU_4(
+	input wire [3:0] P, // adders info
+	input wire [3:0] G, 
+	input wire Cin, // total cin
+	output wire [3:0] C, // adder carry rslt
+	output wire Cout, // for 16-CLU
+	output wire PP, 
+	output wire GG
+); 
+	assign C[0] = Cin; 
+	assign C[1] = G[0] | P[0] & C[0];
+	assign C[2] = G[1] | P[1] & G[0] | P[1] & P[0] & C[0]; 
+	assign C[3] = G[2] | P[2] & G[1] | P[2] & P[1] & G[0] | P[2] & P[1] & P[0] & C[0]; 
+	assign Cout = G[3] | P[3] & G[2] | P[3] & P[2] & G[1] | P[3] & P[2] & P[1] & G[0] | P[3] & P[2] & P[1] & P[0] & C[0]; 
+	assign GG = G[3] | P[3] & G[2] | P[3] & P[2] & G[1] | P[3] & P[2] & P[1] & G[0]; 
+	assign PP = P[3] & P[2] & P[1] & P[0];  
+endmodule
+
+// digital element wrapper
+module bit4_adder(
+	input wire [3:0] A,
+	input wire [3:0] B, 
+	input wire Cin, 
+	output wire [3:0] rslt,
+	output wire Cout,
+	output wire PP, 
+	output wire GG
 );
-	// TODO: Implement this module here
-	
+	wire [3:0] P; 
+	wire [3:0] G; 
+	wire [3:0] C; 
+	CLU_4 CLU_4_0(.P(P), .G(G), .Cin(Cin), .C(C), .Cout(Cout), .PP(PP), .GG(GG)); 
+	bit_full_adder FA_0(.A(A[0]), .B(B[0]), .Cin(C[0]), .P(P[0]), .G(G[0]), .S(rslt[0])); 
+	bit_full_adder FA_1(.A(A[1]), .B(B[1]), .Cin(C[1]), .P(P[1]), .G(G[1]), .S(rslt[1])); 
+	bit_full_adder FA_2(.A(A[2]), .B(B[2]), .Cin(C[2]), .P(P[2]), .G(G[2]), .S(rslt[2])); 
+	bit_full_adder FA_3(.A(A[3]), .B(B[3]), .Cin(C[3]), .P(P[3]), .G(G[3]), .S(rslt[3])); 
+endmodule 
+
+module CLU_16(
+	input wire [3:0] P,
+	input wire [3:0] G, 
+	input wire Cin, 
+	output wire [3:0] C, 
+	output wire Cout
+);
+	assign C[0] = Cin; 
+	assign C[1] = G[0] | P[0] & C[0];
+	assign C[2] = G[1] | P[1] & G[0] | P[1] & P[0] & C[0]; 
+	assign C[3] = G[2] | P[2] & G[1] | P[2] & P[1] & G[0] | P[2] & P[1] & P[0] & C[0]; 
+	assign Cout = G[3] | P[3] & G[2] | P[3] & P[2] & G[1] | P[3] & P[2] & P[1] & G[0] | P[3] & P[2] & P[1] & P[0] & C[0]; 
 endmodule
